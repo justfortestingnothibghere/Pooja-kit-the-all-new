0015ec38c2b671b7e8794ed678feb55b60b978a0
justfortestingnothibghere <armanhacker900@gmail.com>
2025-09-22 19:25:03 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-const sequelize = require('.models//db'); // ✅ import instance
+const sequelize = require('.models/db'); // ✅ import instance
 const User = require('./models/User');
 const Product = require('./models/Product');
 const Order = require('./models/Order');
