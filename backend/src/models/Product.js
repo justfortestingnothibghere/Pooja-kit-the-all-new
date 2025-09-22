b5cf291df955a54ff2f97d0bb1ff725020c933bc
justfortestingnothibghere <armanhacker900@gmail.com>
2025-09-22 19:48:40 +0530

    Update Product.js

--- backend/src/models/Product.js
+++ backend/src/models/Product.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require('sequelize');
-const sequelize = require('../db');
+const sequelize = require('./db');
 
 const Product = sequelize.define('Product', {
   name: { type: DataTypes.STRING, allowNull: false },
