6f42ea62e54ea71e6d561f42671f8a923b83435b
justfortestingnothibghere <armanhacker900@gmail.com>
2025-09-22 19:48:25 +0530

    Update Order.js

--- backend/src/models/Order.js
+++ backend/src/models/Order.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require('sequelize');
-const sequelize = require('../db');
+const sequelize = require('./db');
 const User = require('./User');
 
 const Order = sequelize.define('Order', {
